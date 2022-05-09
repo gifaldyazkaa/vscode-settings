f98ef5bb7ab164246cd7ed6338511511facc67ec
Gifaldy Azka <falcxxdev@pm.me>
2022-05-09 12:00:39 +0700

    feat: 🎸 initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# 📄 VSCode Local Settings
+
+My own VSCode Local settings & configuration
+
+## 🖼️ Preview
+
+[![Settings Preview](https://cdn.upload.systems/uploads/2bQj3LkW.png)](https://i.upload.systems/2bQj3LkW)
+
+## ➡️ Installation
+
+### 🐧 Linux
+
+Default user configuration directory : `~/.config/Code/User`
+
+```bash
+# Copy settings & rename file extension to .json
+$ cp ./settings.ts ~/.config/Code/User/settings.json
+```
+
+## 🧩 Extensions
+
+Install these extensions by copying identifier name to extension search tab
+
+```
+coenraads.bracket-pair-colorizer-2
+bungcip.better-toml
+exodiusstudios.comment-anchors
+icrawl.discord-vscode
+mikestead.dotenv
+github.vscode-pull-request-github
+eamodio.gitlens
+pkief.material-icon-theme
+zhuangtongfa.material-theme
+bmewburn.vscode-intelephense-client
+esbenp.prettier-vscode
+svelte.svelte-vscode
+bradlc.vscode-tailwindcss
+wakatime.vscode-wakatime
+redhat.vscode-yaml
+```
+
+## 📌 Notice
+
+Settings file having `*.ts` extension in order to display settings comment on GitHub. Rename it to `*.json` first if you want to copy it to default user directory or workspace settings.
---
+++ settings.ts
@@ -0,0 +1,53 @@
+{
+  // VSCode app (editor, explorer, files, terminal, workbench, window) settings
+  "editor.formatOnSave": true,
+  "editor.minimap.enabled": false,
+  "editor.inlineSuggest.enabled": true,
+  "editor.cursorBlinking": "phase",
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "editor.fontFamily": "'FiraCode Nerd Font', monospace",
+  "editor.fontLigatures": true,
+  "editor.smoothScrolling": true,
+  "editor.cursorSmoothCaretAnimation": true,
+  "explorer.confirmDragAndDrop": false,
+  "explorer.openEditors.visible": 0,
+  "explorer.compactFolders": false,
+  "files.autoGuessEncoding": true,
+  "terminal.integrated.defaultProfile.windows": "zsh",
+  "terminal.integrated.fontFamily": "'FiraCode Nerd Font', monospace",
+  "terminal.integrated.cursorBlinking": true,
+  "terminal.integrated.cursorStyle": "underline",
+  "workbench.colorTheme": "One Dark Pro Darker",
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.list.smoothScrolling": true,
+  "workbench.sideBar.location": "right",
+  "workbench.startupEditor": "none",
+  "window.dialogStyle": "custom",
+  "window.title": "${dirty}${activeEditorShort}${separator}${rootName}",
+  "window.titleSeparator": "︱",
+  "workbench.tips.enabled": false,
+  "window.menuBarVisibility": "toggle",
+  "window.zoomLevel": -1,
+  // Git settings
+  "git.autofetch": true,
+  "git.fetchOnPull": true,
+  // Language settings
+  "[php]": {
+    "editor.defaultFormatter": "bmewburn.vscode-intelephense-client"
+  },
+  // Extension settings
+  "discord.detailsEditing": "📝 {file_name} | {file_size}",
+  "discord.detailsDebugging": "🐛 {file_name} | {file_size}",
+  "discord.detailsIdling": "💤 | Idling.",
+  "discord.lowerDetailsEditing": "📂 {workspace}",
+  "discord.lowerDetailsDebugging": "📂 {workspace}",
+  "discord.lowerDetailsIdling": "{empty}",
+  "discord.lowerDetailsNoWorkspaceFound": "❌ No Workspace",
+  "discord.largeImage": "{Lang} File",
+  "discord.largeImageIdling": "💤 | Idle",
+  "discord.removeRemoteRepository": true,
+  "bracket-pair-colorizer-2.depreciation-notice": false,
+  // Other settings
+  "php.suggest.basic": false,
+  "npm.packageManager": "yarn"
+}
